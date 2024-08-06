ea54315cc73a9ccf53feea3f063098f9a0a1ed94
Shimanta Mutsuddi <mutsuddi005@gmail.com>
2024-08-07 03:21:11 +0600

    some changes

--- Diff.java
+++ Diff.java
@@ -3,6 +3,6 @@ public class Diff {
     public static void main(String[] args) {
         int a=15;
         int b=5;
-        System.out.println("Diff"+a/b);
+        System.out.println("Diff"+(a-b));
     }
 }
--- Div.java
+++ Div.java
@@ -1,2 +1,8 @@
 public class Div {
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        System.out.println("Div"+(a-b));
+    }
 }
